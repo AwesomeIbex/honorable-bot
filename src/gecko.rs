603d24f0b778482a7cb2cbcf0ae449b203107567
Donovan Dall <donovan_dall@sercomm.com>
2021-04-05 15:56:10 +0100

    better logging

--- src/gecko.rs
+++ src/gecko.rs
@@ -40,23 +40,26 @@ impl Manager<CoingeckoCommand> for CoingeckoConfig {
                     None,
                 );
 
-                if let Ok(mut state) = client.markets(req.clone()).await {
-                    let _ = tx
-                        .send(Command::Discord(DiscordCommand::SendCoingeckoBase(state.clone())))
-                        .await;
+                match client.markets(req.clone()).await {
+                    Ok(mut state) => {
+                        let _ = tx
+                            .send(Command::Discord(DiscordCommand::SendCoingeckoBase(
+                                state.clone(),
+                            )))
+                            .await;
 
-                    loop {
-                        if let Ok(new_state) = client.markets(req.clone()).await {
-                            compare_state(tx.clone(), &state, new_state.clone()).await;
-                            state = new_state;
+                        loop {
+                            if let Ok(new_state) = client.markets(req.clone()).await {
+                                compare_state(tx.clone(), &state, new_state.clone()).await;
+                                state = new_state;
+                            }
+                            tokio::time::sleep(tokio::time::Duration::from_secs(
+                                config_cloned.coingecko.sleep_time_secs,
+                            ))
+                            .await;
                         }
-                        tokio::time::sleep(tokio::time::Duration::from_secs(
-                            config_cloned.coingecko.sleep_time_secs,
-                        ))
-                        .await;
                     }
-                } else {
-                    log::error!("Couldnt start Coingecko loop, couldnt get base state.")
+                    Err(e) => log::error!("Couldnt get base state for coingecko {}", e)
                 }
             }
         });
@@ -67,14 +70,21 @@ async fn compare_state(tx: Sender<Command>, initial_state: &[Market], new_state:
     for market in new_state {
         let market_initial = initial_state.iter().find(|m| m.id == market.id);
         if let Some(market_initial) = market_initial {
-            let has_risen_price = ((market.current_price / market_initial.current_price) * 100_f64) > 20_f64;
-            let raised_ranking = (market.market_cap_rank - market_initial.market_cap_rank) >= 2;
+            let has_risen_price =
+                ((market.current_price / market_initial.current_price) * 100_f64) >= 0_f64;
+            let raised_ranking = (market.market_cap_rank - market_initial.market_cap_rank) >= 0;
 
             if has_risen_price {
-                tx.send(Command::Discord(DiscordCommand::SendCoingeckoPriceIncrease(market.clone()))).await;
+                tx.send(Command::Discord(
+                    DiscordCommand::SendCoingeckoPriceIncrease(market.clone()),
+                ))
+                .await;
             }
             if raised_ranking {
-                tx.send(Command::Discord(DiscordCommand::SendCoingeckoRankIncrease(market))).await;
+                tx.send(Command::Discord(DiscordCommand::SendCoingeckoRankIncrease(
+                    market,
+                )))
+                .await;
             }
         }
     }
